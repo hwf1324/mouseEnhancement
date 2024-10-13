ce5d972c5d071f0c866b771dc5983b60573b6a5c
hwf1324 <1398969445@qq.com>
2024-10-13 21:24:38 +0800

    Ignore the error when entering the security screen because the object does not have the windowClassName attribute.

--- addon/globalPlugins/electron.py
+++ addon/globalPlugins/electron.py
@@ -7,7 +7,6 @@ import controlTypes
 import globalPluginHandler
 import IAccessibleHandler
 import winUser
-from logHandler import log
 from NVDAObjects import NVDAObject
 from NVDAObjects.IAccessible import IAccessible
 from NVDAObjects.IAccessible.chromium import Document
@@ -15,25 +14,31 @@ from NVDAObjects.IAccessible.ia2Web import Ia2Web
 
 
 class RedirectDocument(Ia2Web):
+
 	def objectFromPointRedirect(self, x: int, y: int):
 		docObj: Document = self.previous.lastChild
 		redirect = docObj.IAccessibleObject.accHitTest(x, y)
+
 		if not redirect:
 			return None
+
 		redirect = IAccessibleHandler.normalizeIAccessible(redirect)
 		obj: IAccessible = IAccessible(IAccessibleObject=redirect, IAccessibleChildID=winUser.CHILDID_SELF)
-		log.debug(f"Redirect the {self} at ({x}, {y}) to {obj}")
+
 		return obj
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
 	def chooseNVDAObjectOverlayClasses(self, obj: Ia2Web, clsList: list[NVDAObject]):
-		if (
-			obj.windowClassName.startswith("Chrome_")
-			and obj.role == controlTypes.Role.PANE
-			and obj.childCount == 0
-			and obj.previous
-			and obj.previous.childCount != 0
-			and obj.previous.lastChild.windowClassName == "Chrome_RenderWidgetHostHWND"
-		):
-			clsList.insert(0, RedirectDocument)
+		try:
+			if (
+				obj.windowClassName.startswith("Chrome_")
+				and obj.role == controlTypes.Role.PANE
+				and obj.previous
+				and obj.previous.lastChild
+				and obj.previous.lastChild.windowClassName == "Chrome_RenderWidgetHostHWND"
+			):
+				clsList.insert(0, RedirectDocument)
+		except AttributeError:
+			pass
