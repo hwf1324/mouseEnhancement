11ec855bdf49ae43e0eb3b63ccda6c1ef4981081
hwf1324 <1398969445@qq.com>
2025-01-02 20:44:17 +0800

    Clean up redundant judgment logic.

--- addon/globalPlugins/electron.py
+++ addon/globalPlugins/electron.py
@@ -1,13 +1,12 @@
 # mouseEnhancement add-on for NVDA
 # This file is covered by the GNU General Public License.
 # See the file COPYING.txt for more details.
-# Copyright (C) 2024 hwf1324 <1398969445@qq.com>
+# Copyright (C) 2024-2025 hwf1324 <1398969445@qq.com>
 
 import controlTypes
 import globalPluginHandler
 import IAccessibleHandler
 import winUser
-from NVDAObjects import NVDAObject
 from NVDAObjects.IAccessible import IAccessible
 from NVDAObjects.IAccessible.chromium import Document
 from NVDAObjects.IAccessible.ia2Web import Ia2Web
@@ -30,13 +29,11 @@ class RedirectDocument(Ia2Web):
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
-	def chooseNVDAObjectOverlayClasses(self, obj: Ia2Web, clsList: list[NVDAObject]):
+	def chooseNVDAObjectOverlayClasses(self, obj: Ia2Web, clsList: list):
 		try:
 			if (
 				obj.windowClassName.startswith("Chrome_")
 				and obj.role == controlTypes.Role.PANE
-				and obj.previous
-				and obj.previous.lastChild
 				and obj.previous.lastChild.windowClassName == "Chrome_RenderWidgetHostHWND"
 			):
 				clsList.insert(0, RedirectDocument)
