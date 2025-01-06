8db11c956cecb16afb2549594ddd9255e680fb01
hwf1324 <1398969445@qq.com>
2025-01-06 20:45:44 +0800

    Fix a problem with reporting everything in Windows Terminal.

---
+++ addon/appModules/windowsterminal.py
@@ -0,0 +1,32 @@
+from contextlib import contextmanager
+from typing import Callable
+
+import appModuleHandler
+import controlTypes
+from NVDAObjects import UIA
+from textInfos import UNIT_LINE, UNIT_PARAGRAPH
+
+
+@contextmanager
+def restrictParagraphToLine():
+	import config
+
+	try:
+		curTextUnit = config.conf["mouse"]["mouseTextUnit"]
+		if curTextUnit == UNIT_PARAGRAPH:
+			config.conf["mouse"]["mouseTextUnit"] = UNIT_LINE
+		yield
+	finally:
+		config.conf["mouse"]["mouseTextUnit"] = (
+			curTextUnit or config.conf.getConfigValidation(("mouse", "mouseTextUnit")).default
+		)
+
+
+class AppModule(appModuleHandler.AppModule):
+
+	def event_mouseMove(self, obj: UIA.UIA, nextHandler: Callable[[], None], x: int, y: int) -> None:
+		if obj.role == controlTypes.Role.TERMINAL:
+			with restrictParagraphToLine():
+				nextHandler()
+			return
+		nextHandler()
